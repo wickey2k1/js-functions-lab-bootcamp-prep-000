1c4beceff976317de63f5f36cb841a41a7e06b42
wickert.michael@gmail.com <wickert.michael@gmail.com>
2018-03-27 18:09:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,6 @@ function happyHolidayTo (holiday, name) {
   return (`Happy ${holiday}, ${name}!`);
 }
 
+function holidayCountdown (holiday, days) {
+  return (`It\'s ${days} days until ${holiday}!`);
+}
